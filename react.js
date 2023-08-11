eb8c09ca5d42c70c92011bb5246530a14f6a85ac
Aswini-mca <135096729+Aswini-mca@users.noreply.github.com>
2023-08-11 12:39:20 +0530

    Update react.js

--- react.js
+++ react.js
@@ -2,22 +2,56 @@ import React from "react";
 import "style.css";
 class App extends React.Component {
     render() {
-        const priceCardData = {
+        const priceCardData = [{
             title: 'FREE',
             price: '$0/month',
-            features: ['🗸 Single User', '🗸 50GB Storage', '🗸 Unlimited Public projects', '🗸 Community Access', '⨯ Unlimited Private Projects', '⨯ Dedicated phone Support', '⨯ Free Subdomain', '⨯ Monthly Status Report']
-        };
+            features: ['🗸 Single User', '🗸 50GB Storage', '🗸 Unlimited Public projects', '🗸 Community Access', '⨯ Unlimited Private Projects', '⨯ Dedicated phone Support', '⨯ Free Subdomain', '⨯ Monthly Status Report'],
+        },
+        {
+            title: 'PLUS',
+            price: '$9/month',
+            features: ['🗸 5 Users', '🗸 50GB Storage', '🗸 Unlimited Public projects', '🗸 Community Access', '🗸 Unlimited Private Projects', '🗸 Dedicated phone Support', '🗸 Free Subdomain', '⨯ Monthly Status Report'],
+        },
+        {
+            title: 'PRO',
+            price: '$49/month',
+            features: ['🗸 Unlimited User', '🗸 50GB Storage', '🗸 Unlimited Public projects', '🗸 Community Access', '🗸 Unlimited Private Projects', '🗸 Dedicated phone Support', '🗸 Free Subdomain', '🗸 Monthly Status Report']
+        }];
         return (
             <div className="price-card">
-                <h5>{priceCardData.title}</h5>
-                <p className="card">{priceCardData.price}</p>
-                <p className="f">{priceCardData.features}</p>
-                <button>BUTTON</button>
+                <h1 class="container">React Price Card Task</h1>
+                {priceCardData.map((card) => (
+                    <Card title={card.title} price={card.price} features={card.features} />
+                ))}
             </div>
         );
     }
 }
 export default App;
+
+function Card({ title, price, features }) {
+    return (
+        <div className="head">
+            <div className="card">
+                <p class="text-center" id="tophead">{title}</p>
+                <h2>{price}</h2>
+                <hr />
+                <div class="card-body">
+                    <p class="card-text">{features[0]}</p>
+                    <p class="card-text">{features[1]}</p>
+                    <p class="card-text">{features[2]}</p>
+                    <p class="card-text">{features[3]}</p>
+                    <p class="card-text" id="cross">{features[4]}</p>
+                    <p class="card-text" id="cross">{features[5]}</p>
+                    <p class="card-text" id="cross">{features[6]}</p>
+                    <p class="card-text" id="cross">{features[7]}</p>
+                    <button className="btn">BUTTON</button>
+                </div>
+            </div>
+
+        </div>
+    );
+}
 
 
 
